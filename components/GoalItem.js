9581e56ac7658af6ce49a74ec5962d237d0e1e43
Marcos8060 <marcosgav80@gmail.com>
2024-06-02 09:19:27 +0300

    added android ripple effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,12 +1,15 @@
 import { View, StyleSheet, Text, Pressable } from "react-native";
 
-const GoalItem = ({ itemData,handleDelete }) => {
+const GoalItem = ({ itemData, handleDelete }) => {
   return (
-    <Pressable onPress={handleDelete.bind(this, itemData.item.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={handleDelete.bind(this, itemData.item.id)}
+      >
         <Text style={styles.goalText}>{itemData?.item.name}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -16,11 +19,11 @@ const styles = StyleSheet.create({
   goalItem: {
     marginHorizontal: 8,
     marginVertical: 4,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
+    padding: 8,
     color: "white",
   },
 });
